a5d3260ec89b9b240b0aa2019e9269e2bb2d37b8
ekliot <ekliot@gmail.com>
2018-02-05 19:47:07 -0500

    touchup

--- src/World.cpp
+++ src/World.cpp
@@ -14,6 +14,7 @@
 
 #include "Ray.h"
 #include "Object.h"
+#include "World.h"
 
 World::World() {}
 
--- src/tracerMain.cpp
+++ src/tracerMain.cpp
@@ -16,24 +16,24 @@
 
 #include <SFML/Window.hpp>
 
-#include "Camera.h"
-#include "PPlane.h"
-#include "World.h"
+// #include "Camera.h"
+// #include "PPlane.h"
+// #include "World.h"
 using namespace glm;
 
 // dimensions of drawing window
-int w_width  = 640;
-int w_height = 480;
+const int W_WIDTH  = 640;
+const int W_HEIGHT = 480;
 
 // a pixel buffer of the window dimensions
-uint8_t * pixels[(w_width*w_height)*4];
+uint8_t * pixels[(W_WIDTH*W_HEIGHT)*4];
 
 /**
  * Build the world scene and camera, then render the camera
  * into a pixel buffer
  */
 void init() {
-    World *world = new World();
+    // World *world = new World();
 
     // add objects to the world
 
@@ -42,7 +42,7 @@ void init() {
     //     vec3( 1.0f, 2.53f -7.38f ), // pos TODO this is in world coords, should it be translated somehow?
     //     vec3( 0.0f ), // lookat TODO figure this one out
     //     vec3( 0.0f, 1.0f, 0.0f ), // up TODO figure this one out
-    //     { w_width, w_height, 1.0f } // TODO wtf should the focal length be?
+    //     { W_WIDTH, W_HEIGHT, 1.0f } // TODO wtf should the focal length be?
     // );
 
     // cam.render( pixels );
@@ -53,7 +53,7 @@ int main( void ) {
     // initialize the stuff we need for our rendering
     init();
 
-    sf::Window window( sf::VideoMode( w_width, w_height ), "Ray Tracer" );
+    sf::Window window( sf::VideoMode( W_WIDTH, W_HEIGHT ), "Ray Tracer" );
 
     return 0;
 }
