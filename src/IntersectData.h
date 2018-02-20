5ca51e41d38821964d2354f9a30ef803c5173fc6
ekliot <ekliot@gmail.com>
2018-02-20 14:40:04 -0500

    rename IntersectionData -> IntersectData

---
+++ src/IntersectData.h
@@ -0,0 +1,20 @@
+#ifndef _INTERSECTDATA_H_
+#define _INTERSECTDATA_H_
+
+#include <vector>
+
+#include <glm/vec3.hpp>
+
+#include "Light.h"
+
+using namespace glm;
+
+typedef struct st_IntersectData {
+    vec3 position;
+    vec3 normal;
+    vec3 incoming;
+    std::vector<Light> lights;
+
+} IntersectData;
+
+#endif
