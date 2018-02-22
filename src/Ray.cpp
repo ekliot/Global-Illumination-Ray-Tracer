ed317e5de8e66919f8fff79a2b97a88d7cd628be
Christopher Lemelin <clemelin2@gmail.com>
2018-02-22 10:40:14 -0500

    fixed .gitignore

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -8,7 +8,7 @@
 #endif
 
 #include <iostream>
-
+#include "glm/gtx/string_cast.hpp"
 #include <vector>
 #include <math.h>
 
@@ -16,6 +16,7 @@
 
 using namespace glm;
 #include "Ray.h"
+
 
 Ray::Ray( vec3* start, vec3* dir ) : origin(start), direction(dir) {
     normalizeRay();
@@ -25,7 +26,13 @@ void Ray::normalizeRay(){
     double magnitude = sqrt(this->direction->x * this->direction->x
         + this->direction->y*this->direction->y
         + this->direction->z*this->direction->z);
-    // std::cout << "magnitude: " << magnitude << '\n';
     *(this->direction) = vec3(this->direction->x / magnitude, this->direction->y / magnitude, this->direction->z / magnitude);
 
+}
+
+Ray* Ray::reflectRay(vec3* normal){
+  vec3 newOrigin =  vec3(*this->origin);
+  vec3 newDirectiontemp = reflect(*(this->direction), *(normal));
+
+  return new Ray(&newOrigin,&newDirectiontemp);
 }
