2c5067bc3bafccc178793f85ca929145c82a52b6
ekliot <ekliot@gmail.com>
2018-05-05 11:22:25 -0400

    add kd-tree generation

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -6,31 +6,30 @@
  * Author: ekliot
  */
 
+#include "Camera.h"
+
 #include <iostream>
 
-#include "glm/gtx/string_cast.hpp"
-
-#include "Camera.h"
 #include "Ray.h"
+#include "glm/gtx/string_cast.hpp"
 
 using namespace png;
 using glm::vec3;
 
-Camera::Camera( World* _w, vec3 _pos, vec3 _look, vec3 _up, PPlane _pp ) : world(_w), plane(_pp) {
+Camera::Camera( World* _w, vec3 _pos, vec3 _look, vec3 _up, PPlane _pp )
+    : world( _w ), plane( _pp ) {
     vec3 n = normalize( _look );
-    // vec3 n = normalize( pos - look ); // this is how it should be but we don't like it that way
+    // vec3 n = normalize( pos - look ); // this is how it should be but we
+    // don't like it that way
     vec3 u = normalize( cross( _up, n ) );
     vec3 v = normalize( cross( n, u ) );
 
     // COL-MAJOR
-    transform_mat = mat4(
-        u.x,           v.x,           n.x,           0,
-        u.y,           v.y,           n.y,           0,
-        u.z,           v.z,           n.z,           0,
-        dot(-_pos, u), dot(-_pos, v), dot(-_pos, n), 1
-    );
+    transform_mat =
+        mat4( u.x, v.x, n.x, 0, u.y, v.y, n.y, 0, u.z, v.z, n.z, 0,
+              dot( -_pos, u ), dot( -_pos, v ), dot( -_pos, n ), 1 );
 
-    reverse_transform_mat = inverse(transform_mat);
+    reverse_transform_mat = inverse( transform_mat );
 
     // // ROW-MAJOR
     // transform_mat = mat4(
@@ -41,19 +40,18 @@ Camera::Camera( World* _w, vec3 _pos, vec3 _look, vec3 _up, PPlane _pp ) : world
     // );
 }
 
-Camera::~Camera() {
-    delete world;
-}
+Camera::~Camera() { delete world; }
 
 void Camera::set_scene() {
     if ( !is_set ) {
         world->transform_all( transform_mat );
+        world->generate_kd_tree();
         is_set = true;
     }
 }
 
 void Camera::break_scene() {
-    if ( is_set ){
+    if ( is_set ) {
         world->transform_all( inverse( transform_mat ) );
         is_set = false;
     }
@@ -61,13 +59,14 @@ void Camera::break_scene() {
 
 void Camera::render( image<rgb_pixel>* negative, uint ss_rate ) {
     if ( !is_set ) {
-        std::cout << "Camera::render() called without setting the scene first" << '\n';
+        std::cout << "Camera::render() called without setting the scene first"
+                  << '\n';
         return;
     }
 
     vec3 ray_ori = vec3( 0.0f );
     vec3 ray_dir;
-    Ray *ray;
+    Ray* ray;
     vec3 color;
 
     // width/height of a pixel in world coordinates
@@ -83,32 +82,29 @@ void Camera::render( image<rgb_pixel>* negative, uint ss_rate ) {
     // trace and put stuff into the pixel buffer
     for ( size_t y = 0; y < negative->get_height(); ++y ) {
         for ( size_t x = 0; x < negative->get_width(); ++x ) {
-
             // std::cout << "px {" << x << ',' << y << "}" << '\n';
 
             // if we have a default ss_rate, don't bother with it
             if ( !ss_rate ) {
-                dir_x = start_x + x * px_w + px_w/2;
-                dir_y = start_y - y * px_h - px_h/2;
+                dir_x = start_x + x * px_w + px_w / 2;
+                dir_y = start_y - y * px_h - px_h / 2;
                 dir_z = plane.foc_l;
 
                 ray_dir = vec3( dir_x, dir_y, dir_z );
-                ray = new Ray( &ray_ori, &ray_dir );
+                ray     = new Ray( &ray_ori, &ray_dir );
 
-                color = world->get_intersect( ray, reverse_transform_mat ) * 255.0f;
+                color =
+                    world->get_intersect( ray, reverse_transform_mat ) * 255.0f;
 
-                negative->get_row(y)[x] = rgb_pixel(
-                    int( color.x ),
-                    int( color.y ),
-                    int( color.z )
-                );
+                negative->get_row( y )[x] =
+                    rgb_pixel( int( color.x ), int( color.y ), int( color.z ) );
 
                 delete ray;
             }
             // otherwise, engage super sampling
             else {
                 // how many sub-pixels in each direction
-                uint x_div = ((ss_rate % 2) + ss_rate)/2 + 1;
+                uint x_div = ( ( ss_rate % 2 ) + ss_rate ) / 2 + 1;
                 uint y_div = x_div - ss_rate % 2;
 
                 float r = 0.0f;
@@ -118,16 +114,17 @@ void Camera::render( image<rgb_pixel>* negative, uint ss_rate ) {
                 float base_x = start_x + x * px_w;
                 float base_y = start_y - y * px_h;
 
-                for ( size_t _x = 1; _x <= x_div*2; _x+=2 ) {
-                    for ( size_t _y = 1; _y <= y_div*2; _y+=2 ) {
-                        dir_x = base_x + _x * px_w/(x_div*2);
-                        dir_y = base_y - _y * px_h/(y_div*2);
+                for ( size_t _x = 1; _x <= x_div * 2; _x += 2 ) {
+                    for ( size_t _y = 1; _y <= y_div * 2; _y += 2 ) {
+                        dir_x = base_x + _x * px_w / ( x_div * 2 );
+                        dir_y = base_y - _y * px_h / ( y_div * 2 );
                         dir_z = plane.foc_l;
 
                         ray_dir = vec3( dir_x, dir_y, dir_z );
-                        ray = new Ray( &ray_ori, &ray_dir );
+                        ray     = new Ray( &ray_ori, &ray_dir );
 
-                        color = world->get_intersect( ray, reverse_transform_mat );
+                        color =
+                            world->get_intersect( ray, reverse_transform_mat );
                         r += color.x;
                         g += color.y;
                         b += color.z;
@@ -138,11 +135,10 @@ void Camera::render( image<rgb_pixel>* negative, uint ss_rate ) {
 
                 uint samples = x_div * y_div;
 
-                negative->get_row(y)[x] = rgb_pixel(
-                    int( 255.0f * r / static_cast<float>(samples) ),
-                    int( 255.0f * g / static_cast<float>(samples) ),
-                    int( 255.0f * b / static_cast<float>(samples) )
-                );
+                negative->get_row( y )[x] = rgb_pixel(
+                    int( 255.0f * r / static_cast<float>( samples ) ),
+                    int( 255.0f * g / static_cast<float>( samples ) ),
+                    int( 255.0f * b / static_cast<float>( samples ) ) );
             }
         }
     }
